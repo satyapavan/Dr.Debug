d0d620db02300a1a5d1523437428e83c844cbae1
Satya Pavan <pavan.simham@gmail.com>
2020-06-18 12:50:55 +0530

    added the logging mechanism

--- mr_trouble_service.py
+++ mr_trouble_service.py
@@ -5,6 +5,7 @@ import concurrent.futures
 import cx_Oracle
 import yaml
 import time
+import logging
 
 app = flask.Flask(__name__,
                   static_url_path='',
@@ -13,6 +14,11 @@ app = flask.Flask(__name__,
 app.config['DEBUG'] = True
 app.config['ENV'] = 'development'
 app.config['JSON_KEYS_SORT'] = False
+
+logging.basicConfig(level=logging.DEBUG,
+                    filemode='a',
+                    format='{asctime} - {levelname} - [{funcName}:{lineno}] - {message}',
+                    style='{')
 
 """
 http://127.0.0.1:5000/t-rex/api/query?ENV=SIT&KEY=SVCORDER_ID&VALUE=23/%2045
@@ -27,13 +33,13 @@ def perf(f):
         args_repr = [repr(a) for a in args]
         kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
         signature = ", ".join(args_repr + kwargs_repr)
-        print(f'--> START - Calling {f.__name__}({signature})')
+        logging.debug('--> START - Calling {}({})'.format(f.__name__, signature))
         start_time = time.perf_counter()
         retVal = f(*args, **kwargs)
         end_time = time.perf_counter()
         run_time = end_time - start_time
-        print(f'--> END   - {f.__name__} - in {run_time:.4f} secs')
-        #print(f'--> END   - {f.__name__!r} returned {retVal!r} - in {run_time:.4f} secs')
+        logging.debug('--> END   - {} - in {:.4f} secs'.format(f.__name__, run_time))
+        #logging.debug(f'--> END   - {f.__name__!r} returned {retVal!r} - in {run_time:.4f} secs')
         return retVal
     return _perf
 
@@ -42,7 +48,7 @@ def perf(f):
 @perf
 class TRex:
     def __init__(self, env, key, value):
-        print("Inside Trex constructor")
+        logging.debug("Inside Trex constructor")
         self._env = env
         self._key = key
         self._value = value
@@ -79,42 +85,42 @@ class TRex:
                                                           p_table_wclause,
                                                           self._db_keys[p_key] if p_key in self._db_keys else None)
 
-            print('Query is {}'.format(query))
+            logging.debug('Query is {}'.format(query))
             cur.execute(query)
-            print([row[0] for row in cur.description])
+            logging.debug([row[0] for row in cur.description])
             records = [[row[0] for row in cur.description]]
 
             for itr in cur:
                 records.append(list(itr))
 
-            print(records)
-            print(cur.description)
+            logging.debug(records)
+            logging.debug(cur.description)
 
             return records
 
         except cx_Oracle.Error as error:
-            print(error)
+            logging.error(error)
 
     def _close_all_db_connections(self):
         try:
             for key, value in self._db_conn.items():
-                print(f'Closing the DB connection for {key}')
+                logging.debug(f'Closing the DB connection for {key}')
                 value.close()
         except cx_Oracle.Error as error:
-            print(error)
+            logging.error(error)
 
     def _open_db_connection(self, list_of_domain_conn):
         try:
             domain_name, db_conn = list_of_domain_conn
-            print("Inside ", list_of_domain_conn)
+            logging.debug("Inside {}".format(list_of_domain_conn))
 
             connection = cx_Oracle.connect(db_conn)
-            print('DB Connection = {}'.format(db_conn))
+            logging.debug('DB Connection = {}'.format(db_conn))
 
             self._db_conn[domain_name] = connection
-            print(self._db_conn)
+            logging.debug(self._db_conn)
         except cx_Oracle.Error as error:
-            print(error)
+            logging.error(error)
 
     @perf
     def _open_db_connections_loop(self):
@@ -130,7 +136,7 @@ class TRex:
                         executor.map(self._open_db_connection, list(documents[self._env].items()))
 
         except Exception as error:
-            print(error)
+            logging.error(error)
 
     @perf
     def _fetch_records_per_domain(self, domain_name, list_tables):
@@ -138,7 +144,7 @@ class TRex:
             if domain_name in self._db_conn:
                 connection = self._db_conn[domain_name]
                 # show the version of the Oracle Database
-                print(connection.version)
+                logging.debug(connection.version)
                 cur = connection.cursor()
 
                 for itrTables in list_tables:
@@ -146,9 +152,9 @@ class TRex:
                     table_key = itrTables['KEY'] if 'KEY' in itrTables else None
                     table_wclause = itrTables['EXPLICIT_WCLAUSE'] if 'EXPLICIT_WCLAUSE' in itrTables else None
 
-                    print('Table name is {}'.format(table_name))
-                    print('Table key is {}'.format(table_key))
-                    print('Table wClause is {}'.format(table_wclause))
+                    logging.debug('Table name is {}'.format(table_name))
+                    logging.debug('Table key is {}'.format(table_key))
+                    logging.debug('Table wClause is {}'.format(table_wclause))
 
                     db_results = self._fetch_records_per_table(cur, table_name, table_key, table_wclause)
                     if domain_name not in self._results:
@@ -156,10 +162,10 @@ class TRex:
 
                     self._results[domain_name][table_name] = db_results
 
-                print(self._results)
+                logging.debug(self._results)
 
         except cx_Oracle.Error as error:
-            print(error)
+            logging.error(error)
 
     @perf
     def processAPI(self):
@@ -172,7 +178,7 @@ class TRex:
             if 'domains' in documents:
                 for itrDomains in documents['domains']:
                     domain_name = itrDomains['name'] if 'name' in itrDomains else None
-                    print('Domain name is {}'.format(domain_name))
+                    logging.debug('Domain name is {}'.format(domain_name))
                     if 'tables' in itrDomains:
                         self._fetch_records_per_domain(domain_name, itrDomains['tables'])
 
@@ -184,26 +190,25 @@ class TRex:
 @perf
 @app.route('/', methods=['GET'])
 def root():
-    print('Entering root')
+    logging.debug('Entering root')
     return app.send_static_file('index.html')
 
 # TODO - Try making this a json input rather than a query parameter
 @perf
 @app.route('/t-rex/api/query', methods=['GET'])
 def query():
-    #print_request(request)
 
     ####################################################################################################################
-    print('Is json Request: {}'.format(request.is_json))
+    logging.debug('Is json Request: {}'.format(request.is_json))
     try:
         if request.args:
             p_env = request.args['ENV'] if 'ENV' in request.args else 'None'
             p_key = request.args['KEY'] if 'KEY' in request.args else 'None'
             p_value = request.args['VALUE'] if 'VALUE' in request.args else 'None'
 
-        print('type(p_env) = {} - value = {}'.format(type(p_env), p_env) )
-        print('type(p_key) = {} - value = {}'.format(type(p_key), p_key))
-        print('type(p_value) = {} - value = {}'.format(type(p_value), p_value))
+        logging.debug('type(p_env) = {} - value = {}'.format(type(p_env), p_env) )
+        logging.debug('type(p_key) = {} - value = {}'.format(type(p_key), p_key))
+        logging.debug('type(p_value) = {} - value = {}'.format(type(p_value), p_value))
 
         if p_env == 'None' or p_key == 'None' or p_value == 'None' :
             ## TODO - see if we can comeup with an exception class and properly passon the message. currently, status code is missing
@@ -213,50 +218,27 @@ def query():
     ####################################################################################################################
 
         objTRex = TRex(p_env, p_key, p_value)
-        print(jsonify(objTRex.results()))
-        print(objTRex.results())
-        print(objTRex)
-        print(objTRex.__dict__)  ## This is also an alternative to use rather than overriding __str__
+        logging.debug(jsonify(objTRex.results()))
+        logging.debug(objTRex.results())
+        logging.debug(objTRex)
+        logging.debug(objTRex.__dict__)  ## This is also an alternative to use rather than overriding __str__
         objTRex.processAPI()
 
     ####################################################################################################################
 
     except Exception as e:
-        print('Error: {}'.format(e))
+        logging.error('Error: {}'.format(e))
         raise jsonify('Error: {}'.format(e))
     else:
-        print('There is NO exception')
+        logging.debug('There is NO exception')
 
     return jsonify(objTRex.results())
 
-def print_request(request):
-    host = request.host
-    print(f"host: {host}")
-
-    host_url = request.host_url
-    print(f"host_url: {host_url}")
-
-    path = request.path
-    print(f"path: {path}")
-
-    full_path = request.full_path
-    print(f"full_path: {path}")
-
-    url = request.url
-    print(f"url: {url}")
-
-    base_url = request.base_url
-    print(f"base_url: {base_url}")
-
-    url_root = request.url_root
-    print(f"url_root: {url_root}")
-
-    headers = request.headers
-    print(headers)
-
 def main():
-    print("Hello!")
+    logging.debug("Hello!")
     app.config["DEBUG"] = True
+
+    logging.debug("Hello {}".format('World'))
 
 if __name__ == "__main__": main()
 
